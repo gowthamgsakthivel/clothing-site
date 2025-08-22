4e0d97a41fd48448e11a19ecb9e759f79ce15cf5
gowthamgsakthivel <gowthamgsakthivel@gmail.com>
2025-08-22 23:29:25 +0530

    added order functionality with inngest

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -1,12 +1,16 @@
+
+import { inngest } from "@/config/inngest";
 import Product from "@/models/Product";
+import User from "@/models/User";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-
+import connectDB from "@/config/db";
 
 
 
 export async function POST(request) {
     try {
+        await connectDB();
         const { userId } = getAuth(request);
         const { address, items } = await request.json();
 
@@ -20,7 +24,26 @@ export async function POST(request) {
             return acc + product.offerPrice * item.quantity;
         }, 0);
 
+        await inngest.send({
+            name: 'order/created',
+            data: {
+                userId,
+                address,
+                items,
+                amount: amount + Math.floor(amount * 0.02),
+                date: Date.now()
+            }
+        })
+
+        // clear user cart
+        const user = await User.findById(userId)
+        user.cartItems = {}
+        await user.save()
+
+        return NextResponse.json({ success: true, message: 'Order Placed' });
+
     } catch (error) {
-        return NextResponse.json({ success: false });
+        console.log(error);
+        return NextResponse.json({ success: false, message: error.message });
     }
 }
