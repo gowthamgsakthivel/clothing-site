65bbdde448e04d6ba1bc7927d44cd38431a60479
gowthamgsakthivel <gowthamgsakthivel@gamil.com>
2025-08-24 00:44:53 +0530

    added create order functionality

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,5 +1,7 @@
+
 import { serve } from "inngest/next";
 import { createUserOrder, inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
+import Order from "@/models/Orders"; // Ensure Order model is loaded for Inngest
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
