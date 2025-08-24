7c5d107da183e27736dcaadda9c250ef935d31ad
gowthamgsakthivel <gowthamgsakthivel@gamil.com>
2025-08-24 15:28:37 +0530

    added order listing functionalities

---
+++ app/api/order/list/route.js
@@ -0,0 +1,30 @@
+import connectDB from "@/config/db";
+import Address from "@/models/Address";
+import Order from "@/models/Orders";
+import Product from "@/models/Product";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+
+
+
+export async function GET(request) {
+    try {
+        const { userId } = getAuth(request);
+
+        await connectDB()
+
+        Address.length
+        Product.length
+
+        const orders = await Order.find({ userId })
+            .populate("address")
+            .populate("items.product");
+
+        return NextResponse.json({ success: true, orders });
+
+    } catch (error) {
+
+        return NextResponse.json({ success: false, error: error.message });
+    }
+}
